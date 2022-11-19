336a9e28a7f36d45a4b6b64866738c21a4de6237
SantiTeran <89756153+SantiTeran@users.noreply.github.com>
2022-11-19 13:21:03 -0300

    Fix constants

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -1,6 +1,6 @@
-const POKEMON = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40`;
+const POKEMON = `https://pokeapi.co/api/v2/pokemon?offset=20&limit=20`;
 const TYPE = `https://pokeapi.co/api/v2/type`
 
-// console.log(URL_KEY)
+
 
 module.exports = {POKEMON,TYPE};
