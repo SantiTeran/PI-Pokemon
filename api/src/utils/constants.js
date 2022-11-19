8e6ce4ceb3aee41c921a7a942613615565f01370
SantiTeran <89756153+SantiTeran@users.noreply.github.com>
2022-11-19 13:10:53 -0300

    Fix constants

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -1,6 +1,6 @@
-const { API_KEY } = process.env
-const URL_KEY = `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`;
+const POKEMON = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40`;
+const TYPE = `https://pokeapi.co/api/v2/type`
 
 // console.log(URL_KEY)
 
-module.exports = {URL_KEY};
+module.exports = {POKEMON,TYPE};
