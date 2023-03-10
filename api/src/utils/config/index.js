4ac4e403c0eceafebe3bdc74440ab85f9fe5fff3
SantiTeran <89756153+SantiTeran@users.noreply.github.com>
2023-03-10 12:42:05 -0300

    deploy

--- api/src/utils/config/index.js
+++ api/src/utils/config/index.js
@@ -4,5 +4,5 @@ module.exports = {
     dbUser:process.env.DB_USER || 'postgres',
     dbPassword:process.env.DB_PASSWORD,
     dbHost:process.env.DB_HOST || 'localhost',
-    PORT:process.env.PORT || '3001',
+    PORT:process.env.PORT 
 }
